e1be16a41481b02d7c01529ecab888f1b21b11f3
Matthew <matthewhannemann@yahoo.com>
2024-11-06 21:09:49 -0800

    Add files via upload

---
+++ Inheritance.py
@@ -0,0 +1,17 @@
+
+
+class = User:
+    name = 'no Name Provided'
+    email = ' '
+    passowrd = '1234567890'
+    account_number = 0
+
+
+class = Supplier:
+    item = Pasta
+    catagory = Food
+
+class = Customer(User):
+    mailing_address = ' '
+    mailing_list = True
+    
